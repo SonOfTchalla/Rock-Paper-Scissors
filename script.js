eb2b03249a5c49ee38e464e39ab70210bbcbccc0
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2024-06-25 12:43:33 +0200

    Move score variables and playRound function
    Move playRound function and score variables so they are declared inside the new playGame function

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 //rock paper scissors game
 
-//keep a score
-let humanScore = 0;
-let computerScore = 0;
-
 //keep a round count
 
 //get the computer choice
@@ -29,31 +25,6 @@ function getHumanChoice() {
     }
 }
 
-//play a single round
-function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
-
-    if(computerChoice === "rock" && humanChoice === "scissors"){
-        computerScore++;
-        return `You lost! ${computerChoice} beats ${humanChoice}`;
-    }else if(computerChoice === "paper" && humanChoice === "rock"){
-        computerScore++;
-        return `You lost! ${computerChoice} beats ${humanChoice}`;
-    }else if(computerChoice === "scissors" && humanChoice === "paper"){
-        computerScore++;
-        return `You lost! ${computerChoice} beats ${humanChoice}`;
-    }else if(computerChoice === humanChoice){
-        return `${computerChoice} ties ${humanChoice}`;
-    }else if(humanChoice === 'invalid input'){
-        return 'invalid input! Please choose a valid option';
-    } 
-    else{
-        humanScore++;
-        return `You win! ${humanChoice} beats ${computerChoice}`;
-    }
-
-}
-
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice()
 
@@ -62,7 +33,34 @@ console.log(`Computer chose: ${computerSelection}`);
 console.log(playRound(humanSelection, computerSelection));
 
 function playGame(){
-    
+    //keep a score
+    let humanScore = 0;
+    let computerScore = 0;
+
+    //play a single round
+    function playRound(humanChoice, computerChoice){
+        humanChoice = humanChoice.toLowerCase();
+
+        if(computerChoice === "rock" && humanChoice === "scissors"){
+            computerScore++;
+            return `You lost! ${computerChoice} beats ${humanChoice}`;
+        }else if(computerChoice === "paper" && humanChoice === "rock"){
+            computerScore++;
+            return `You lost! ${computerChoice} beats ${humanChoice}`;
+        }else if(computerChoice === "scissors" && humanChoice === "paper"){
+            computerScore++;
+            return `You lost! ${computerChoice} beats ${humanChoice}`;
+        }else if(computerChoice === humanChoice){
+            return `${computerChoice} ties ${humanChoice}`;
+        }else if(humanChoice === 'invalid input'){
+            return 'invalid input! Please choose a valid option';
+        } 
+        else{
+            humanScore++;
+            return `You win! ${humanChoice} beats ${computerChoice}`;
+        }
+
+    }
 }
 
 //delcare the winner
