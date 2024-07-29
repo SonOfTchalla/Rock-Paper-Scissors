a1b468f57e1f812ee0eb94da0a00d61e76d769f1
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2024-07-29 16:23:18 +0200

    Add event listeners for buttons

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 //rock paper scissors game
 
+//keep a score
+let humanScore = 0;
+let computerScore = 0;
 
 //Query selectors
 let rockEl = document.querySelector("#rock")
 let paperEl = document.querySelector("#paper")
 let scissorsEl = document.querySelector("#scissors")
 
+//Event listeners
+rockEl.addEventListener("click", () => {
+    let userChoice = "rock";
+    const computerSelection = getComputerChoice()
+    playRound(userChoice, computerSelection);
+})
+
+paperEl.addEventListener("click", () => {
+    let userChoice = "paper";
+    const computerSelection = getComputerChoice()
+    playRound(userChoice, computerSelection);
+})
+
+scissorsEl.addEventListener("click", () => {
+    let userChoice = "scissors";
+    const computerSelection = getComputerChoice()
+    playRound(userChoice, computerSelection);
+})
 
 //get the computer choice
 function getComputerChoice () {
@@ -20,74 +41,37 @@ function getComputerChoice () {
     }    
 }
 
-//get the human choice
-function getHumanChoice() {
-    let userChoice = prompt("Choose one of the following options: Rock, Paper or Scissors. What is your choice?");
-    if(userChoice.toLowerCase() === "rock" || userChoice.toLowerCase() === "paper" || userChoice.toLowerCase() === "scissors"){
-        return userChoice;
-    }else{
-        return 'invalid input';
+//play a single round
+function playRound(humanChoice, computerChoice){
+    //humanChoice = humanChoice.toLowerCase();
+
+    console.log(`You chose ${humanChoice}`)
+    console.log(`Computer chose ${computerChoice}`)
+    if(computerChoice === "rock" && humanChoice === "scissors"){
+        computerScore++;
+        console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
+    }else if(computerChoice === "paper" && humanChoice === "rock"){
+        computerScore++;
+        console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
+    }else if(computerChoice === "scissors" && humanChoice === "paper"){
+        computerScore++;
+        console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
+    }else if(computerChoice === humanChoice){
+        console.log(`${computerChoice} ties ${humanChoice}`);
+    }else if(humanChoice === 'invalid input'){
+        console.log('invalid input! Please choose a valid option');
+    } 
+    else{
+        humanScore++;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
     }
+
+    console.log(`Human Score: ${humanScore}`)
+    console.log(`Computer Score: ${computerScore}`)
 }
 
-function playGame(){
-    //keep a score
-    let humanScore = 0;
-    let computerScore = 0;
+    
 
-    //play a single round
-    function playRound(humanChoice, computerChoice){
-        humanChoice = humanChoice.toLowerCase();
-
-        if(computerChoice === "rock" && humanChoice === "scissors"){
-            computerScore++;
-            console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
-        }else if(computerChoice === "paper" && humanChoice === "rock"){
-            computerScore++;
-            console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
-        }else if(computerChoice === "scissors" && humanChoice === "paper"){
-            computerScore++;
-            console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
-        }else if(computerChoice === humanChoice){
-            console.log(`${computerChoice} ties ${humanChoice}`);
-        }else if(humanChoice === 'invalid input'){
-            console.log('invalid input! Please choose a valid option');
-        } 
-        else{
-            humanScore++;
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        }
-
-    }
     
     
-            const humanSelection = getHumanChoice();
-            const computerSelection = getComputerChoice()
-
-            //console.log(`-------Round ${i+1}-------`);
-            console.log(`You chose: ${humanSelection.toLowerCase()}`);
-            console.log(`Computer chose: ${computerSelection}`); 
-
-            playRound(humanSelection, computerSelection);
-            
-            console.log(' ');
-            console.log(`User Score: ${humanScore}`);
-            console.log(`Computer Score: ${computerScore}`);
-            console.log(' ');
-
-        
-
-        if(humanScore > computerScore)
-        {
-            console.log("You win!");
-        }else if(humanScore == computerScore){
-            console.log("It's a tie");
-        }else{
-            console.log("You lose!");
-        }
-
-}
-
-
-//function call
-playGame();
+    
